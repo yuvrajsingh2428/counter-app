df47dcba0b25f310eec29b7947e6c99ca954d06d
yuvrajsingh2428 <offcyuvi2428@gmail.com>
2024-08-22 14:14:22 +0530

    first commit

---
+++ counter.js
@@ -0,0 +1,31 @@
+let counter = 0
+
+const counterValue = document.getElementById("counter-value");
+const incrementBtn = document.getElementById("increment-btn");
+const decrementBtn = document.getElementById("decrement-btn");
+const resetBtn = document.querySelector('#reset')
+
+// to increment the value of counter
+
+incrementBtn.addEventListener('click', () =>{
+    counter++;
+    counterValue.innerHTML = counter;
+})
+
+//To decrement the value of counter
+decrementBtn.addEventListener('click', () => {
+    counter --;
+    counterValue.innerHTML = counter;
+})
+
+// To reset the value
+
+resetBtn.addEventListener('click', ()=>{
+    counter = 0;
+    counterValue.innerHTML = counter;
+})
+
+
+
+
+
